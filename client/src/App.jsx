8b74be31cb513d68eeee833688f5ebeb08b1ca5a
CodingAesthete <harry_olli@abv.bg>
2024-06-20 17:10:53 +0300

    Pages and Routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,19 @@
-import React from 'react'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home';
+import About from './pages/About';
+import Contacts from './pages/Contacts';
+import LearnMore from './pages/LearnMore';
 
 export default function App() {
   return (
-    <div>App</div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/learn-more" element={<LearnMore />} />
+        <Route path="/contacts" element={<Contacts />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
