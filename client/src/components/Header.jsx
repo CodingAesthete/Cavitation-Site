f963946c5e8c393434a24d118d6b9b87cd93d6bf
CodingAesthete <harry_olli@abv.bg>
2024-06-23 01:22:34 +0300

    Create new sections and change Company Logo

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,50 +1,52 @@
-import React from 'react'
-import { Link } from "react-router-dom"
-import Logo from "../assets/images/logo-site.png"
-import Avatar from "../assets/images/site-avatar.jpg"
-import { useSelector } from 'react-redux'
+import React from 'react';
+import { Link } from "react-router-dom";
+import OfficialLogo from '../assets/images/OfficialLogo.png';
+import Avatar from "../assets/images/site-avatar.jpg";
+import { useSelector } from 'react-redux';
+import { motion } from 'framer-motion';
 
 export default function Header() {
   const { currentUser } = useSelector((state) => state.user);
   return (
     <header className='bg-blue-200 text-black font-semibold shadow-lg'>
-      <div className='flex items-center justify-between p-2 lg:gap-4 sm:mx-10'>
+      <div className='flex items-center justify-between lg:gap-4 sm:mx-28'>
         <Link to='/'>
-          <div className='flex items-center gap-2 sm:gap-6'>
-            <div className='w-8 sm:w-14'>
-              <img src={Logo} alt="Logo" className='shadow-lg rounded-full' />
+          <div className='flex items-center gap-2 sm:gap-4'>
+            <div className='w-10 sm:w-16'>
+              <img src={OfficialLogo} alt="Logo" className='' />
             </div>
-            <div className='text-lg sm:text-3xl text-center font-bold text-black whitespace-nowrap' style={{ letterSpacing: '3px' }}>
-              Кавитация БГ
-            </div>
+            <motion.div
+              className='text-lg sm:text-3xl text-center font-bold text-black whitespace-nowrap'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.2 }}
+              style={{
+                letterSpacing: '3px',
+                fontFamily: 'Pacifico, cursive',
+                background: 'linear-gradient(to right, #1e3c72, #2a5298)',
+                WebkitBackgroundClip: 'text',
+                WebkitTextFillColor: 'transparent'
+              }}
+            >
+              Vortex Shipping
+            </motion.div>
           </div>
-
         </Link>
         <ul className='flex items-center gap-0 sm:gap-8 text-lg lg:text-xl sm:whitespace-nowrap'>
           <Link to='/'>
-            <li className='hidden lg:inline hover:underline cursor-pointer'>Начало</li>
+            <li className='hidden lg:inline hover:underline cursor-pointer'>За нас</li>
           </Link>
-          <Link to='/about'>
-            <li className='hover:underline'>Технологии</li>
+          <Link to='/learn-more'>
+            <li className='hidden sm:inline hover:underline'>Кавитация</li>
           </Link>
           <Link to='/goals'>
             <li className='hidden xl:inline hover:underline'>Нашите цели</li>
           </Link>
-          <Link to='/learn-more'>
-            <li className='hidden sm:inline hover:underline'> Научете повече</li>
-          </Link>
           <Link to='/contacts'>
             <li className='hidden lg:inline hover:underline'> Контакти </li>
           </Link>
-          <Link to='/sign-in'><li className='inline hover:underline cursor-pointer'><img className=' w-11 bg-blue-200 rounded-full' src={Avatar} alt="avatar icon" /></li>
-          </Link>
-          {/* <Link to='/profile'>
-            {currentUser ? (
-              <li className='inline hover:underline cursor-pointer'>SignIn</li>
-            ) : (<li className='inline hover:underline cursor-pointer'><img className=' w-11 bg-blue-200 rounded-full' src={Avatar} alt="avatar icon" /></li>)}
-          </Link> */}
         </ul>
       </div>
     </header>
-  )
+  );
 }
--- client/src/pages/Contacts.jsx
+++ client/src/pages/Contacts.jsx
@@ -3,7 +3,11 @@ import { motion } from 'framer-motion';
 import Message from '../assets/images/message.webp';
 import Telephone from '../assets/images/telephone.png';
 import MapImage from '../assets/images/Map.jpg';
-import SubjectOfActivity from '../assets/images/SubjectOfActivity.jpg';
+import AddressIcon from '../assets/images/AddressIcon.jpg';
+import Check from '../assets/images/Check.png';
+import SuccessIcon from '../assets/images/SuccessIcon.png';
+import ReputationIcon from '../assets/images/Reputation.webp';
+import Vision from '../assets/images/Vision2.jpg';
 
 export default function LearnMore() {
   return (
@@ -11,106 +15,133 @@ export default function LearnMore() {
       initial={{ opacity: 0 }}
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
-      className="text-lg text-justify w-5/6 mx-auto rounded-md px-10 py-6 mt-6 mb-10 bg-blue-50"
+      className="text-lg text-justify w-5/6 mx-auto rounded-md px-10 py-6 mt-6 mb-6 bg-blue-50"
     >
       <div>
         <motion.h1
-          className='w-full text-center text-3xl font-bold pb-6'
+          className='w-full text-center text-3xl font-bold pb-6 mb-6'
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
           transition={{ delay: 0.2 }}
         >
           Свържете се с нас
         </motion.h1>
-        <div className="flex flex-col md:flex-row gap-6">
-          <div className="md:w-3/5">
-            <motion.img
-              src={Message}
-              alt="Contact icon"
-              style={{ float: 'left', maxWidth: '150px', marginRight: '6px' }}
-              whileHover={{ scale: 1.1 }}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.2 }}
-            />
-            <motion.p
-              className='mb-3'
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.3 }}
-            >
-              С удоволствие ще отговорим на всички ваши въпроси и запитвания относно нашите услуги и продукти. Независимо дали искате да направите поръчка, да получите допълнителна информация или просто да споделите своите идеи и предложения, ние сме тук, за да ви помогнем.
-            </motion.p>
-            <motion.p
-              className='mb-3 flex items-center'
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.3 }}
-            >
-              <img
-                src={Telephone}
-                alt="Telephone icon"
-                style={{ width: '24px', height: '24px', marginRight: '8px' }}
+        <div className="flex flex-col gap-6">
+          <div className="flex flex-col md:flex-row gap-6 w-full">
+            <div className="w-full">
+              <motion.img
+                src={Message}
+                alt="Contact icon"
+                className="float-left max-w-xs mr-4"
+                whileHover={{ scale: 1.1 }}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.2 }}
               />
-              <b>+359-88-661-2166</b>&nbsp;-&nbsp;<i>Георги Стоянов Мишков</i>
-            </motion.p>
-            <motion.p
-              className='mb-3 flex items-center'
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              transition={{ delay: 0.3 }}
-            >
-              <img
-                src={Telephone}
-                alt="Telephone icon"
-                style={{ width: '24px', height: '24px', marginRight: '8px' }}
-              />
-              гр.Варна, р-н Приморски, Ген.Радко Димитриев 13, вх.Б, ет.1
-            </motion.p>
+              <motion.p
+                className='mb-3'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                С удоволствие ще отговорим на всички ваши въпроси и запитвания относно нашите услуги и продукти. Независимо дали искате да направите поръчка, да получите допълнителна информация или просто да споделите своите идеи и предложения, ние сме тук, за да ви помогнем.
+              </motion.p>
+              <motion.p
+                className='mb-3 flex items-center'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <img
+                  src={Telephone}
+                  alt="Telephone icon"
+                  className="w-6 h-6 mr-2"
+                />
+                <b>+359-88-661-2166</b>&nbsp;-&nbsp;<i>Георги Стоянов Мишков</i>
+              </motion.p>
+
+            </div>
           </div>
-          <div className="flex-1 md:w-2/5">
+          <div className="flex justify-between items-center">
+            <div>
+              <motion.p
+                className='mb-3 flex'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <img
+                  src={AddressIcon}
+                  alt="Address icon"
+                  className="w-6 h-6 mr-2"
+                />
+                гр.Варна, р-н Приморски, Ген.Радко Димитриев 13, вх.Б, ет.1
+              </motion.p>
+              <motion.p
+                className='mb-3 flex'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <img
+                  src={Check}
+                  alt="Check icon"
+                  className="w-6 h-6 mr-2"
+                />
+                Статусът ни е активен и сме в процес на надграждане
+              </motion.p>
+              <motion.p
+                className='mb-3 flex'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <img
+                  src={SuccessIcon}
+                  alt="Success icon"
+                  className="w-6 h-6 mr-2"
+                />
+                Дългогодишен опит и успешни проекти
+              </motion.p>
+              <motion.p
+                className='mb-3 flex'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <img
+                  src={ReputationIcon}
+                  alt="Reputation icon"
+                  className="w-6 h-6 mr-2"
+                />
+                Доволни клиенти и безупречна репутация
+              </motion.p>
+              <motion.p
+                className='mb-3 flex'
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ delay: 0.3 }}
+              >
+                <img
+                  src={Vision}
+                  alt="Vision icon"
+                  className="w-6 h-6 mr-2"
+                />
+                Амбициозни цели и дългосрочна визия
+              </motion.p>
+            </div>
             <motion.img
               src={MapImage}
               alt="Map"
-              style={{ width: '100%', maxHeight: 'auto', borderRadius: '8px' }}
+              className="w-5/12 max-h-full rounded-md"
               initial={{ opacity: 0 }}
               animate={{ opacity: 1 }}
               transition={{ delay: 0.4 }}
             />
+
           </div>
         </div>
       </div>
-      <div className="mt-6 flex items-center">
-        <img
-          src={SubjectOfActivity}
-          alt="Subject of Activity"
-          className="mr-4"
-          style={{ maxWidth: '300px' }}
-        />
-        <motion.div>
-          <motion.h1
-            className='w-full text-center text-xl font-semibold pb-6 text-blue-900'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            transition={{ delay: 0.2 }}
-          >
-            <i>Предмет на дейност</i>
-          </motion.h1>
-          <p className="text-lg leading-8 italic">
-            - търговски сделки пряко и непряко свързани с морско, речно и каботажно плаване;<br />
-            - маркетинг, внос и износ, лизингова дейност,<br />
-            - търговско представителство и посредничество,<br />
-            - складови сделки,<br />
-            - сделки с интелектуална собственост, покупка на стоки или други вещи <br />
-            - продажба на стоки от собствено производство, комисионни, спедиционни и превозни сделки, менителници, записи на заповед и чекове,<br />
-            - лицензионни сделки, стоков контрол, покупка, проектиране, строителство,<br />
-            - ремонт или обзавеждане на недвижими имоти с цел продажба или отдаване под наем, земеделие и животновъдство;<br />
-            - еко земеделие, проектиране, производство и търговия с иновативни производствени екосистеми системи;<br />
-            - внедряване в производствени екосистеми за пречистване на бункеровани танкерни води, пречиствателни корабни и наземни станции за пречистване на морски и отпадъчни води.
-          </p>
-
-        </motion.div>
-      </div>
-    </motion.div>
+    </motion.div >
   );
 }
