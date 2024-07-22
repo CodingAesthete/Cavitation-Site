0f5ac6a7c5a0d599bb4bc1b1d91e6ea264cf8fa7
CodingAesthete <harry_olli@abv.bg>
2024-07-22 08:32:51 +0300

    Add Translation Effects

--- client/src/pages/Effects.jsx
+++ client/src/pages/Effects.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef, useState, useEffect } from "react";
+import { motion, useInView } from 'framer-motion';
 import Water1 from '../assets/images/water1.jpg';
 import Water2 from '../assets/images/water2.jpg';
 import Water6 from '../assets/images/water6.jpg';
@@ -20,8 +20,29 @@ import WaterInitial from '../assets/images/WaterInitial.jpg';
 import { useTranslation, Trans } from 'react-i18next';
 import i18n from '../i18n'; // Import i18n instance
 
+function useMediaQuery() {
+  const [screenSize, setScreenSize] = React.useState('');
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) {
+        setScreenSize('sm');
+      } else {
+        setScreenSize('lg');
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return screenSize;
+}
+
 export default function Effects() {
-  const { t } = useTranslation();
   const albumPhotoContainerStyle = {
     border: '8px solid #6B4226', // Brown border
     borderRadius: '8px', // Rounded corners
@@ -100,6 +121,29 @@ export default function Effects() {
     );
   };
 
+  const screenSize = useMediaQuery();
+
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { once: true });
+  const { t } = useTranslation();
+
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const handleLanguageChange = () => {
+      setIsAnimating(true);
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 200); // Duration of the animation in milliseconds
+    };
+
+    i18n.on('languageChanged', handleLanguageChange);
+
+    return () => {
+      i18n.off('languageChanged', handleLanguageChange);
+    };
+  }, []);
+
   return (
     <motion.div
       variants={pageVariants}
@@ -109,184 +153,190 @@ export default function Effects() {
       className="text-lg text-justify w-11/12 sm:w-5/6 mx-auto rounded-md px-3 sm:px-10 py-6 mt-6 mb-6 bg-blue-50 relative"
       style={{ zIndex: 2 }} // Ensure this has a higher z-index than WaterDrops
     >
-      <WaterDrops />
-      <div className="p-6min-h-screen">
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT1")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT1P1")}
-          </p>
-          <ul className="list-disc list-inside pl-4 animate-slideInUp mb-4">
-            <li>{t("effT1P2")}</li>
-            <li>{t("effT1P3")}</li>
-          </ul>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img src={Water1} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle}>
-              <img src={WaterInitial} alt="Water 2" className=" max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT1P4")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT2")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT2P1")}
-          </p>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={Snowflake} alt="Snowflake" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={WaterStructure} alt="Water Structure" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT2P2")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT3")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT3P1")}
-          </p>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={net} alt="net" className=" max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={Water6} alt="Water 1" className="hidden md:block max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT3P2")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT4")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT4P1")}
-          </p>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={Water7} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={Water8} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT4P2")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT5")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT5P1")}
-          </p>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={Water9} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={Water10} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT5P2")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT6")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT6P1")}
-          </p>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={BlackHorse} alt="Black Horse" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={Cow} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT6P2")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT7")}
-          </h2>
-          <p className="mb-4 animate-slideInUp md:text-justify text-center">
-            {t("effT7P1")}
-          </p>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={Water19} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
-            </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={Water20} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT7P2")}
-          </p>
-        </section>
-
-        <section className="mb-8">
-          <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
-            {t("effT8")}
-          </h2>
-          <div className="animate-slideInUp">
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: isAnimating ? 0 : 1, scale: isAnimating ? 0.95 : 1 }}
+        transition={{ duration: 0.6 }}
+      >
+        <WaterDrops />
+        <div className="p-6min-h-screen">
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT1")}
+            </h2>
             <p className="mb-4 animate-slideInUp md:text-justify text-center">
-              {t("effT8P1")}
+              {t("effT1P1")}
             </p>
-            <p className="mb-4 animate-slideInUp md:text-justify text-center">
-              {t("effT8P2")}
-            </p>
-            <p className="mb-4 animate-slideInUp md:text-justify text-center">
-              {t("effT8P3")}
-            </p>
-            <p className="mb-4 animate-slideInUp md:text-justify text-center">
-              {t("effT8P4")}
-            </p>
-          </div>
-          <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
-            <div style={albumPhotoContainerStyle}>
-              <img loading="lazy" src={Byala1} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
+            <ul className="list-disc list-inside pl-4 animate-slideInUp mb-4">
+              <li>{t("effT1P2")}</li>
+              <li>{t("effT1P3")}</li>
+            </ul>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img src={Water1} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle}>
+                <img src={WaterInitial} alt="Water 2" className=" max-w-sx md:w-md border rounded-md" />
+              </div>
             </div>
-            <div style={albumPhotoContainerStyle} className="hidden md:block">
-              <img loading="lazy" src={Byala2} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
-            </div>
-          </div>
-          <p className="text-center text-md italic">
-            {t("effT8P5")}
-          </p>
-        </section>
+            <p className="text-center text-md italic">
+              {t("effT1P4")}
+            </p>
+          </section>
 
-      </div>
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT2")}
+            </h2>
+            <p className="mb-4 animate-slideInUp md:text-justify text-center">
+              {t("effT2P1")}
+            </p>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={Snowflake} alt="Snowflake" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={WaterStructure} alt="Water Structure" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT2P2")}
+            </p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT3")}
+            </h2>
+            <p className="mb-4 animate-slideInUp md:text-justify text-center">
+              {t("effT3P1")}
+            </p>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={net} alt="net" className=" max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={Water6} alt="Water 1" className="hidden md:block max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT3P2")}
+            </p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT4")}
+            </h2>
+            <p className="mb-4 animate-slideInUp md:text-justify text-center">
+              {t("effT4P1")}
+            </p>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={Water7} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={Water8} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT4P2")}
+            </p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT5")}
+            </h2>
+            <p className="mb-4 animate-slideInUp md:text-justify text-center">
+              {t("effT5P1")}
+            </p>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={Water9} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={Water10} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT5P2")}
+            </p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT6")}
+            </h2>
+            <p className="mb-4 animate-slideInUp md:text-justify text-center">
+              {t("effT6P1")}
+            </p>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={BlackHorse} alt="Black Horse" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={Cow} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT6P2")}
+            </p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT7")}
+            </h2>
+            <p className="mb-4 animate-slideInUp md:text-justify text-center">
+              {t("effT7P1")}
+            </p>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={Water19} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={Water20} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT7P2")}
+            </p>
+          </section>
+
+          <section className="mb-8">
+            <h2 className="text-2xl font-bold mb-4 text-center animate-fadeInSlow">
+              {t("effT8")}
+            </h2>
+            <div className="animate-slideInUp">
+              <p className="mb-4 animate-slideInUp md:text-justify text-center">
+                {t("effT8P1")}
+              </p>
+              <p className="mb-4 animate-slideInUp md:text-justify text-center">
+                {t("effT8P2")}
+              </p>
+              <p className="mb-4 animate-slideInUp md:text-justify text-center">
+                {t("effT8P3")}
+              </p>
+              <p className="mb-4 animate-slideInUp md:text-justify text-center">
+                {t("effT8P4")}
+              </p>
+            </div>
+            <div className="flex flex-col md:flex-row justify-between items-center mb-4 md:gap-8">
+              <div style={albumPhotoContainerStyle}>
+                <img loading="lazy" src={Byala1} alt="Water 1" className="max-w-sx md:w-md border rounded-md" />
+              </div>
+              <div style={albumPhotoContainerStyle} className="hidden md:block">
+                <img loading="lazy" src={Byala2} alt="Water 2" className="hidden md:block max-w-sx md:w-md border rounded-md" />
+              </div>
+            </div>
+            <p className="text-center text-md italic">
+              {t("effT8P5")}
+            </p>
+          </section>
+
+        </div>
+      </motion.div>
     </motion.div>
   );
 }
--- client/src/pages/Goals.jsx
+++ client/src/pages/Goals.jsx
@@ -1,11 +1,55 @@
-import React, { useState } from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef, useState, useEffect } from "react";
+import { motion, useInView } from 'framer-motion';
 import { FaChevronDown } from 'react-icons/fa';
 import CavitationVideo from '../assets/images/cavitation-video.mp4';
 import { useTranslation, Trans } from 'react-i18next';
 import i18n from '../i18n'; // Import i18n instance
 
+function useMediaQuery() {
+  const [screenSize, setScreenSize] = React.useState('');
+
+  React.useEffect(() => {
+    const handleResize = () => {
+      if (window.innerWidth < 640) {
+        setScreenSize('sm');
+      } else {
+        setScreenSize('lg');
+      }
+    };
+
+    handleResize();
+
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return screenSize;
+}
+
 export default function Goals() {
+  const screenSize = useMediaQuery();
+
+  const sectionRef = useRef(null);
+  const isInView = useInView(sectionRef, { once: true });
+
+  const [isAnimating, setIsAnimating] = useState(false);
+
+  useEffect(() => {
+    const handleLanguageChange = () => {
+      setIsAnimating(true);
+      setTimeout(() => {
+        setIsAnimating(false);
+      }, 200); // Duration of the animation in milliseconds
+    };
+
+    i18n.on('languageChanged', handleLanguageChange);
+
+    return () => {
+      i18n.off('languageChanged', handleLanguageChange);
+    };
+  }, []);
+
   const [selectedQuestion, setSelectedQuestion] = useState(null);
   const [showOverlay, setShowOverlay] = useState(false); // State for showing overlay
   const { t } = useTranslation();
@@ -89,61 +133,67 @@ export default function Goals() {
       exit={{ opacity: 0 }}
       className="text-lg text-justify w-11/12 sm:w-5/6 mx-auto rounded-md  px-3 sm:px-10 py-6 mt-6 mb-6 bg-blue-50"
     >
-      <h1 className='text-2xl font-bold mb-4 text-center'>{t("factT1")}</h1>
-      <div className='space-y-4'>
-        {questionsAndAnswers.map((qa, index) => (
-          <div key={index} className='border-b pb-2 border-darkblue-500'>
-            <button
-              onClick={() => handleQuestionClick(index)}
-              className='text-lg font-medium text-left w-full focus:outline-none flex items-center justify-between'
-            >
-              <span className='flex sm:items-center'>
-                <span className='bg-gradient-to-r from-blue-500 to-violet-500 text-white font-bold mr-2 flex-shrink-0 w-8 h-8 rounded-full flex items-center justify-center'>
-                  {index + 1}
-                </span>
-                <span className=' ml-1 sm:ml-2 sm:text-justify md:text-start'> {qa.question}</span>
-              </span>
-
-              {/* <FaChevronDown className={`transform transition-transform duration-300 ${selectedQuestion === index ? 'rotate-180' : ''} sm:hidden`} /> */}
-            </button>
-
-            <div className="flex justify-center cursor-pointer mt-2" onClick={() => handleQuestionClick(index)}>
-              <FaChevronDown className={`transform transition-transform duration-300 ${selectedQuestion === index ? 'rotate-180' : ''}`} />
-            </div>
-            {selectedQuestion === index && (
-              <motion.div
-                variants={answerVariants}
-                initial='hidden'
-                animate='visible'
-                className='mt-2 text-gray-700 text-center sm:text-justify'
+      <motion.div
+        initial={{ opacity: 1 }}
+        animate={{ opacity: isAnimating ? 0 : 1, scale: isAnimating ? 0.95 : 1 }}
+        transition={{ duration: 0.6 }}
+      >
+        <h1 className='text-2xl font-bold mb-4 text-center'>{t("factT1")}</h1>
+        <div className='space-y-4'>
+          {questionsAndAnswers.map((qa, index) => (
+            <div key={index} className='border-b pb-2 border-darkblue-500'>
+              <button
+                onClick={() => handleQuestionClick(index)}
+                className='text-lg font-medium text-left w-full focus:outline-none flex items-center justify-between'
               >
-                {qa.answer.split('').map((letter, idx) => (
-                  <motion.span key={idx} variants={letterVariants}>
-                    {letter}
-                  </motion.span>
-                ))}
-              </motion.div>
-            )}
-          </div>
-        ))}
-      </div>
+                <span className='flex sm:items-center'>
+                  <span className='bg-gradient-to-r from-blue-500 to-violet-500 text-white font-bold mr-2 flex-shrink-0 w-8 h-8 rounded-full flex items-center justify-center'>
+                    {index + 1}
+                  </span>
+                  <span className=' ml-1 sm:ml-2 sm:text-justify md:text-start'> {qa.question}</span>
+                </span>
 
-      {/* Video component with animated overlay */}
-      <div style={{ position: 'relative', maxWidth: '100%', maxHeight: 'auto', margin: '10px auto' }}>
-        <motion.video
-          src={CavitationVideo}
-          type="video/mp4"
-          autoPlay
-          loop
-          muted
-          style={{ maxWidth: '100%', display: 'block' }}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ delay: 0.2 }}
-          onMouseEnter={() => setShowOverlay(true)}
-          onMouseLeave={() => setShowOverlay(false)}
-        />
-      </div>
+                {/* <FaChevronDown className={`transform transition-transform duration-300 ${selectedQuestion === index ? 'rotate-180' : ''} sm:hidden`} /> */}
+              </button>
+
+              <div className="flex justify-center cursor-pointer mt-2" onClick={() => handleQuestionClick(index)}>
+                <FaChevronDown className={`transform transition-transform duration-300 ${selectedQuestion === index ? 'rotate-180' : ''}`} />
+              </div>
+              {selectedQuestion === index && (
+                <motion.div
+                  variants={answerVariants}
+                  initial='hidden'
+                  animate='visible'
+                  className='mt-2 text-gray-700 text-center sm:text-justify'
+                >
+                  {qa.answer.split('').map((letter, idx) => (
+                    <motion.span key={idx} variants={letterVariants}>
+                      {letter}
+                    </motion.span>
+                  ))}
+                </motion.div>
+              )}
+            </div>
+          ))}
+        </div>
+
+        {/* Video component with animated overlay */}
+        <div style={{ position: 'relative', maxWidth: '100%', maxHeight: 'auto', margin: '10px auto' }}>
+          <motion.video
+            src={CavitationVideo}
+            type="video/mp4"
+            autoPlay
+            loop
+            muted
+            style={{ maxWidth: '100%', display: 'block' }}
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ delay: 0.2 }}
+            onMouseEnter={() => setShowOverlay(true)}
+            onMouseLeave={() => setShowOverlay(false)}
+          />
+        </div>
+      </motion.div>
     </motion.div>
   );
 }
